8f72c42c5e84e8e9a90f5abae69534e51afbf745
billyyan <By869422>
2022-11-14 00:14:12 -0800

    Modified MealActivity.kt

---
+++ app/src/main/java/com/example/easyfood/MealActivity.kt
@@ -0,0 +1,104 @@
+package com.example.easyfood
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.example.easyfood.databinding.ActivityMealBinding
+import com.example.easyfood.fragments.HomeFragment
+import com.example.easyfood.pojo.Meal
+import com.example.easyfood.viewModel.HomeViewModel
+import com.example.easyfood.viewModel.MealViewModel
+
+class MealActivity : AppCompatActivity() {
+
+    private lateinit var mealId: String
+    private lateinit var mealName: String
+    private lateinit var mealThumb: String
+    private lateinit var binding: ActivityMealBinding
+    private lateinit var youtubeLink: String
+    private lateinit var mealMvvm: MealViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMealBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mealMvvm = ViewModelProvider(this)[MealViewModel::class.java]
+
+        getMealInformationFromIntent()
+
+        setInformationInViews()
+
+        loadingCase()
+        mealMvvm.getMealDetail(mealId)
+        observeMealDetailLiveData()
+
+        onYoutubeImageClick()
+    }
+
+    private fun onYoutubeImageClick() {
+        binding.imgYoutube.setOnClickListener {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(youtubeLink))
+            startActivity(intent)
+        }
+    }
+
+    private fun observeMealDetailLiveData() {
+        mealMvvm.observeMealDetailsLiveData().observe(this, object: Observer<Meal> {
+            override fun onChanged(t: Meal?) {
+                onResponseCase()
+                val meal = t
+
+                binding.tvCategoryInfo.text = "Category : ${meal!!.strCategory}"
+                binding.tvAreaInfo.text = "Area : ${meal.strArea}"
+                binding.tvInstructions.text = meal.strInstructions
+
+                youtubeLink = meal.strYoutube
+            }
+
+        })
+    }
+
+    private fun setInformationInViews() {
+        Glide.with(applicationContext)
+            .load(mealThumb)
+            .into(binding.imgMealDetail)
+
+        binding.collapsingToolbar.title = mealName
+        binding.collapsingToolbar.setCollapsedTitleTextColor(ContextCompat.getColor(applicationContext, R.color.white))
+        binding.collapsingToolbar.setExpandedTitleColor(ContextCompat.getColor(applicationContext, R.color.white))
+    }
+
+    private fun getMealInformationFromIntent() {
+        val intent = intent
+        mealId = intent.getStringExtra(HomeFragment.MEAL_ID)!!
+        mealName = intent.getStringExtra(HomeFragment.MEAL_NAME)!!
+        mealThumb = intent.getStringExtra(HomeFragment.MEAL_THUMB)!!
+
+    }
+
+    private fun loadingCase() {
+        binding.progressBar.visibility = View.VISIBLE
+        binding.btnAddToFav.visibility = View.INVISIBLE
+        binding.tvInstructions.visibility = View.INVISIBLE
+        binding.tvCategoryInfo.visibility = View.INVISIBLE
+        binding.tvAreaInfo.visibility = View.INVISIBLE
+        binding.imgYoutube.visibility = View.INVISIBLE
+    }
+
+    private fun onResponseCase() {
+        binding.progressBar.visibility = View.INVISIBLE
+        binding.btnAddToFav.visibility = View.VISIBLE
+        binding.tvInstructions.visibility = View.VISIBLE
+        binding.tvCategoryInfo.visibility = View.VISIBLE
+        binding.tvAreaInfo.visibility = View.VISIBLE
+        binding.imgYoutube.visibility = View.VISIBLE
+
+    }
+}
--- app/src/main/java/com/example/easyfood/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/easyfood/fragments/HomeFragment.kt
@@ -1,29 +1,30 @@
 package com.example.easyfood.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import com.bumptech.glide.Glide
-import com.example.easyfood.R
+import com.example.easyfood.MealActivity
 import com.example.easyfood.databinding.FragmentHomeBinding
 import com.example.easyfood.pojo.Meal
-import com.example.easyfood.pojo.MealList
-import com.example.easyfood.retrofit.RetrofitInstance
 import com.example.easyfood.viewModel.HomeViewModel
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
     private lateinit var homeMvvm: HomeViewModel
+    private lateinit var randomMeal: Meal
+
+    companion object {
+        const val MEAL_ID = "com.example.easyfood.fragments.idMeal"
+        const val MEAL_NAME = "com.example.easyfood.fragments.nameMeal"
+        const val MEAL_THUMB = "com.example.easyfood.fragments.thumbMeal"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,15 +45,27 @@ class HomeFragment : Fragment() {
 
         homeMvvm.getRandomMeal()
         observeRandomMeal()
+        onRandomMealClick()
+    }
+
+    private fun onRandomMealClick() {
+        binding.randomMealCard.setOnClickListener {
+            val intent = Intent(activity, MealActivity::class.java)
+            intent.putExtra(MEAL_ID, randomMeal.idMeal)
+            intent.putExtra(MEAL_NAME, randomMeal.strMeal)
+            intent.putExtra(MEAL_THUMB, randomMeal.strMealThumb)
+            startActivity(intent)
+        }
     }
 
     private fun observeRandomMeal() {
-        homeMvvm.observeRandomMealLivedata().observe(viewLifecycleOwner,object: Observer<Meal> {
-            override fun onChanged(t: Meal?) {
-                Glide.with(this@HomeFragment)
-                    .load(t!!.strMealThumb)
-                    .into(binding.imgRandomMeal)
-            }
-        })
+        homeMvvm.observeRandomMealLivedata().observe(viewLifecycleOwner
+        ) { meal ->
+            Glide.with(this@HomeFragment)
+                .load(meal!!.strMealThumb)
+                .into(binding.imgRandomMeal)
+
+            this.randomMeal = meal
+        }
     }
 }
